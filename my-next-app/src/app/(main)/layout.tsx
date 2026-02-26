57b9d3664677465b32a009ffde84a71e66bec612
campus3 S018 <s018@alpacoedu.onmicrosoft.com>
2026-02-26 09:01:01 +0900

    주식 가격 데이터 테이블 추가

--- my-next-app/src/app/(main)/layout.tsx
+++ my-next-app/src/app/(main)/layout.tsx
@@ -1,9 +1,11 @@
+import AppHeader from '@/components/layouts/app-header';
 import type { ReactNode } from 'react';
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <div>MainLayout</div>
+      <AppHeader />
 
       {children}
     </div>
